6d65008ce86b15dd3882ef89430d72176e3143a9
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-06-21 11:20:56 +0900

    Geocoding API commit

--- map.php
+++ map.php
@@ -1,126 +1,34 @@
-<!DOCTYPE html>
+<?php
+
+echo
+file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDg17cdXyTvjl2aLn4vADfWgtUAzEWDR4I");
+
+?>
+
 <html>
   <head>
-    <title>Geolocation</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
+    <title>Geocoding An Address</title>
   </head>
   <body>
-    <div id="map"></div>
-    <script>
-      // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-              // alert("緯度：" + pos["lat"] + "軽度：" + pos["lng"]);
-            infoWindow.setPosition(pos);
-            // infoWindow.setContent('Location found.');
-            infoWindow.setContent("緯度：" + pos["lat"] + "軽度：" + pos["lng"]);
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+  <script type="text/javascript">
+
+    var position = {};
+    $.ajax({
+      url:"https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDg17cdXyTvjl2aLn4vADfWgtUAzEWDR4I",
+      type: "GET",
+      success: function(data){
+        // console.log(data);
+        $.each(data['results'][0],["address_components"], function(key, value){
+          // alert(key);
+          if(value["types"][0] == "country"){
+            alert(value["long_name"]);
+          }
+        })
       }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpcF5uFMC5EOCaxEx4Qf1F8wVKEJ7eMG4&callback=initMap">
-    </script>
+    })
+  </script>
   </body>
 </html>
-<!-- <!DOCTYPE html>
-<html>
-  <head>
-    <title>Simple Map</title>
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/maps/documentation/javascript/demos/demos.css">
-    <style>
-    html, body {
-      height: 100%;
-      margin: 0;
-      padding: 0;
-    }
-    #map {
-      height: 100%;
-    }
-
-    </style>
-  </head>
-  <body>
-    <div id="map"></div> -->
-    <!-- <script>
-    function initMap() {
-      var london = {lat: 51.50, lng: -0.11};
-      var birmngham = {lat: 52.48, lng: -1.89};
-    }
-
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: london,
-        scrollwheel: false,
-        zoom: 7
-        });
-
-
-      var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-
-      var request = {
-        destination: birmingham,
-        origin: london,
-        travelMode: 'DRIVING'
-      };
-
-      var directionsService = new google.maps.DirectionsService();
-      directionsService.route(request, function(response, status){
-        if (status == 'OK') {
-          directionDisplay.setDirections(response);
-        }
-      });
-    }
-
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpcF5uFMC5EOCaxEx4Qf1F8wVKEJ7eMG4&callback=initMap" async defer>
-    </script> -->
-  <!-- </body>
-</html> -->
